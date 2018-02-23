0bb86702174e9928d70a82fd376fe930aee37dda
vanessasoutoc <vanessasoutoc@gmail.com>
2018-02-23 15:47:27 -0300

    mongo config db

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 
 module.exports = {
-	connectionString: 'mongodb://localhost:27017/bravolog'
+	connectionString: 'mongodb://auth-nodejs-express:auth1234@ds147518.mlab.com:47518/auth-nodejs-express'
 }
--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -6,7 +6,7 @@ const controller = require('../controllers/user-controller');
 const authService = require('../services/auth-service');
 const aclService = require('../services/acl-service');
 
-router.post('/', authService.authorize, aclService.preFilter('USUARIO'), controller.create);
+router.post('/', /* authService.authorize, aclService.preFilter('USUARIO'), */ controller.create);
 //router.post('/', controller.create);
 
 module.exports = router;
